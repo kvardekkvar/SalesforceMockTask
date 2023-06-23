4eb4ccbcb2d34adcb643d53edb448b24ff1cd184
Alexander Matveev <sa-matik@yandex.com>
2023-06-23 21:06:09 +0300

    Add markup for components (HTML & CSS)

--- force-app/main/default/lwc/order_manager/order_manager.js
+++ force-app/main/default/lwc/order_manager/order_manager.js
@@ -1,14 +1,34 @@
 import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
+import { getRecord } from 'lightning/uiRecordApi';
+
+    const FIELDS = [
+    'Account.Name',
+    'Account.AccountNumber'
+    ];
 
 export default class Order_manager extends LightningElement {
+
 
     @wire(CurrentPageReference)
     setCurrentPageReference(currentPageReference) {
         this.currentPageReference = currentPageReference;
     }
+
+    @wire(getRecord, { recordId: "$accountId", fields: FIELDS })
+    account;
+
+
     get accountId() {
         return this.currentPageReference?.state?.c__accountId;
+    }
+
+    get accountName(){
+        return this.account.data?.fields.Name.value;
+    }
+
+    get accountNumber(){
+        return this.account.data?.fields.AccountNumber.value;
     }
 
 }
